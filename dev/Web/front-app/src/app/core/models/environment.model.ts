46dd0004c8a73e7b5a20c11b79e442e6441e0b69
youssef-khalifi <youssefkhalifi767@gmail.com>
2025-06-04 12:48:09 +0200

    create environment service

--- dev/Web/front-app/src/app/core/models/environment.model.ts
+++ dev/Web/front-app/src/app/core/models/environment.model.ts
@@ -1,8 +1,47 @@
 import {KeyValuePair} from './request.model';
 
 export interface Environment {
-  id: string;
+  id: number;
   name: string;
-  variables: KeyValuePair[];
-  isActive: boolean;
+  workSpaceId: number;
+  variables: { [key: string]: string };
+  createdAt: Date;
+  updatedAt?: Date;
+  createdBy?: string;
+  updatedBy?: string;
+  lastSyncDate?: Date;
+  syncId: string;
+}
+
+export interface CreateEnvironmentRequest {
+  name: string;
+  workSpaceId: number;
+  variables?: { [key: string]: string };
+}
+
+export interface UpdateEnvironmentRequest {
+  id: number;
+  name: string;
+}
+
+export interface AddVariableToEnvironmentRequest {
+  environmentId: number;
+  key: string;
+  value: string;
+}
+
+export interface UpdateVariableInEnvironmentRequest {
+  environmentId: number;
+  key: string;
+  value: string;
+}
+
+export interface AddVariablesToEnvironmentRequest {
+  environmentId: number;
+  variables: { [key: string]: string };
+}
+
+export interface RemoveVariableFromEnvironmentRequest {
+  environmentId: number;
+  key: string;
 }
